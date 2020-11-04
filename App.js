d80ca4e6cd93aca4b8696ab0fa352c2052de76db
shadman-ahmed-bs23 <shadmanahmed874@gmail.com>
2020-11-04 20:32:08 +0600

    Drawer Navigation added

--- App.js
+++ App.js
@@ -2,42 +2,19 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { Button, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-//import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import HomeStackScreen from './navigations/HomeStackScreen'; 
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import SettingsStackScreen from './navigations/SettingsStackScreen';
+import TabNavigator from './navigations/TabNavigator';
 
-const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'home'
-                : 'home-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'settings' : 'settings-outline';
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: '#6d50ad',
-          inactiveTintColor: '#6d50ad',
-        }}
-      >
-        <Tab.Screen name="Home" component={HomeStackScreen} />
-        <Tab.Screen name="Settings" component={SettingsStackScreen} />
-      </Tab.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={TabNavigator} />
+        <Drawer.Screen name="Contact" component={SettingsStackScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
--- navigations/TabNavigator.js
+++ navigations/TabNavigator.js
@@ -1,17 +1,39 @@
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { HomeStackNavigator, AboutStackNavigator } from "./StackNavigator";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import HomeStackScreen from './HomeStackScreen'; 
+import SettingsStackScreen from './SettingsStackScreen';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeStackNavigator} />
-      <Tab.Screen name="Contact" component={AboutStackNavigator} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = focused
+              ? 'home'
+              : 'home-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings' : 'settings-outline';
+          }
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: '#6d50ad',
+        inactiveTintColor: '#6d50ad',
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeStackScreen} />
+      <Tab.Screen name="Settings" component={SettingsStackScreen} />
     </Tab.Navigator>
   );
-};
+}
 
-export default TabNavigator;
+export default TabNavigator; 
